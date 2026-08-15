8236eec96bf07b9211ed1aece422a3194ebcf3d1
Yashwanth Chilaka <yashwanth.chilaka@gmail.com>
2026-08-15 16:32:35 +0530

    Screenshare setup

--- hypr/.config/hypr/hyprland/on_boot.lua
+++ hypr/.config/hypr/hyprland/on_boot.lua
@@ -3,7 +3,9 @@ require("hyprland.variables")
 function startup_apps()
     return function()
         hl.exec_cmd("noctalia")
+        hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
         hl.exec_cmd("systemctl --user start hyprpolkitagent")
+        hl.exec_cmd("~/dotfiles/scripts/desktop-portal")
 
         hl.exec_cmd(code);
         hl.exec_cmd([[sleep 3 && hyprctl dispatch 'hl.dsp.window.move({workspace="2", follow=false, window="class:code"})']]);
