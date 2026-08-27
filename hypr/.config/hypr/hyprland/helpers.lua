54ac453aadbc2a41f6120189c9c05575e75ea2be
Yashwanth Chilaka <yashwanth.chilaka@gmail.com>
2026-08-27 18:37:31 +0530

    removed debug

--- hypr/.config/hypr/hyprland/helpers.lua
+++ hypr/.config/hypr/hyprland/helpers.lua
@@ -1,5 +1,4 @@
 function Get_Monitor_Name(index)
-    hl.notification.create({text = "called", timeout=2000})
     local monitors = hl.get_monitors()
     if index > #monitors then
         return nil
