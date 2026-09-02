5b51789b5081b8fca9dfdc6ef1cd389ada3978bb
Yashwanth Chilaka <yashwanth.chilaka@gmail.com>
2026-09-02 15:38:46 +0530

    Fullscreen when one window

--- hypr/.config/hypr/hyprland/workspace_rules.lua
+++ hypr/.config/hypr/hyprland/workspace_rules.lua
@@ -2,7 +2,7 @@ require("hyprland.variables")
 require("hyprland.helpers")
 
 -- Fullscreen when only one window in workspace
-hl.workspace_rule({ workspace = "w[1]", decorate = false, gaps_in = 5, gaps_out = 5, no_border = true })
+hl.workspace_rule({ workspace = "w[1]", decorate = false, gaps_in = 0, gaps_out = 0, no_border = true })
 hl.workspace_rule({ workspace = "w[(f)2]", decorate = false, gaps_in = 5, gaps_out = 5, no_border = true })
 
 local internal_monitor = Get_Monitor_Name(1)
@@ -13,8 +13,8 @@ hl.workspace_rule({workspace = "2", monitor = external_monitor, persistent = tru
 
 -- Special Workspace rules
 hl.workspace_rule({workspace = "special:term", on_created_empty="[tile]kitty"})
-hl.workspace_rule({workspace = "special:music", on_created_empty = audioplyr, decorate = false, gaps_in = 0, gaps_out = 0, no_border = true })
-hl.workspace_rule({workspace = "special:textin", on_created_empty = textin, decorate = false, gaps_in = 0, gaps_out = 0, no_border = true })
+hl.workspace_rule({workspace = "special:music", on_created_empty = audioplyr })
+hl.workspace_rule({workspace = "special:textin", on_created_empty = textin })
 hl.workspace_rule({workspace = "special:wrapped_discord", on_created_empty = discord_wrapper})
 hl.workspace_rule({workspace = "special:fmgr", on_created_empty = "thunar"})
 hl.workspace_rule({workspace = "special:stream", on_created_empty = tube})
