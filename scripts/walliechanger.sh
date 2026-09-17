a24cf4a44ba374f60b024cb8a1c6bbce23e8c5ca
Yashwanth Chilaka <yashwanth.chilaka@gmail.com>
2026-09-17 14:44:35 +0530

    wallie script fix

--- scripts/walliechanger.sh
+++ scripts/walliechanger.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 time=$(date +"%H")
 if (( $time >= 8 && $time < 19)) then
-	noctalia msg wallpaper-set '/home/yashwanthchilaka/Downloads/wallies/eldenring/sorceress_sellen_elden_ring.png'
+	noctalia msg wallpaper-set '~/Downloads/wallies/eldenring/sorceress_sellen_elden_ring.png'
 else
-	noctalia msg wallpaper-set '/home/yashwanthchilaka/Downloads/wallies/eldenring/fia_the_deathbed_companion.png'
+	noctalia msg wallpaper-set '~/Downloads/wallies/eldenring/fia_the_deathbed_companion.png'
 fi
